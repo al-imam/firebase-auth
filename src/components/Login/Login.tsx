8751952bbec6ff2314bb6044bc44d8b66fd30d36
al-imam <alimam01828@gmail.com>
2022-12-26 00:54:32 +0600

    formatting Login component
      - extract input to separate component
      - extract font import to global file
      - formate names

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,5 +1,23 @@
-function Login() {
-  return <h1>Your in login</h1>;
-}
+import Input from "../Input/Input";
+import classes from "./login.module.css";
+
+const Login: React.FunctionComponent = () => {
+  return (
+    <form className={classes.form}>
+      <Input type="text" placeholder="Enter email address" />
+      <Input type="password" placeholder="Enter password" />
+      <button className={classes.log} type="submit">
+        Log in
+      </button>
+      <a href="#" className={classes.forget}>
+        Forget password?
+      </a>
+      <hr className={classes.line} />
+      <button className={classes.newAccount} type="button">
+        Create New Account
+      </button>
+    </form>
+  );
+};
 
 export default Login;
