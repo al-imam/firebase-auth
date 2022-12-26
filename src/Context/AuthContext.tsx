50191f811cb1b484c229ee7f4950d7c88cb7b2c4
al-imam <alimam01828@gmail.com>
2022-12-27 01:24:59 +0600

    config auth
      - create context
      - create useAuth
      - create singUp function

---
+++ src/Context/AuthContext.tsx
@@ -0,0 +1,51 @@
+import {
+  createUserWithEmailAndPassword,
+  getAuth,
+  onAuthStateChanged,
+  User,
+  UserCredential,
+} from "firebase/auth";
+import { createContext, useContext, useEffect, useState } from "react";
+import app from "../firebase";
+
+const auth = getAuth(app);
+
+interface ValueType {
+  currentUser: User | null;
+  singUp: (email: string, password: string) => Promise<UserCredential>;
+}
+
+const AuthContext = createContext<ValueType | null>(null);
+
+export const useAuth = (): ValueType | null => {
+  return useContext(AuthContext);
+};
+
+interface AuthContextProps {
+  children: React.ReactNode;
+}
+
+export const AuthProvider: React.FunctionComponent<AuthContextProps> = ({
+  children,
+}) => {
+  const [currentUser, setCurrentUSer] = useState<User | null>(null);
+
+  function singUp(email: string, password: string): Promise<UserCredential> {
+    return createUserWithEmailAndPassword(auth, email, password);
+  }
+
+  useEffect(() => {
+    const unSubscribe = onAuthStateChanged(auth, (user) => {
+      setCurrentUSer(user);
+      console.log(user);
+    });
+
+    return unSubscribe;
+  }, []);
+
+  const value = {
+    currentUser,
+    singUp,
+  };
+  return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
+};
