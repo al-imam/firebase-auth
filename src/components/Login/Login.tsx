d8a9ac92ed4218fd534c693bc695afbd97dc1947
al-imam <alimam01828@gmail.com>
2022-12-26 18:40:46 +0600

    login form done

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,19 +1,19 @@
 import Anchor from "../Anchor/Anchor";
 import Button from "../Button/Button";
+import Form from "../Form/Form";
 import Hr from "../Hr/Hr";
 import Input from "../Input/Input";
-import classes from "./login.module.css";
 
 const Login: React.FunctionComponent = () => {
   return (
-    <form className={classes.form}>
+    <Form>
       <Input type="text" placeholder="Enter email address" />
       <Input type="password" placeholder="Enter password" />
       <Button text="Log in" />
       <Anchor text="Forget password" />
       <Hr />
       <Anchor text="Create new account" variant="button" />
-    </form>
+    </Form>
   );
 };
 
