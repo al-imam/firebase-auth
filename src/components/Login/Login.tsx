bfafe3641e66f436ad351f6e7ffc9865e8514c0b
al-imam <alimam01828@gmail.com>
2022-12-26 17:16:55 +0600

    extract Hr to won component

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,5 +1,6 @@
 import Anchor from "../Anchor/Anchor";
 import Button from "../Button/Button";
+import Hr from "../Hr/Hr";
 import Input from "../Input/Input";
 import classes from "./login.module.css";
 
@@ -10,7 +11,7 @@ const Login: React.FunctionComponent = () => {
       <Input type="password" placeholder="Enter password" />
       <Button text="Log in" />
       <Anchor text="Forget password" />
-      <hr className={classes.line} />
+      <Hr />
       <Anchor text="Create new account" variant="button" />
     </form>
   );
