14af7b6bec819fe7bce63a4df03d2ff5ded444c4
al-imam <alimam01828@gmail.com>
2022-12-26 17:00:07 +0600

    style Anchor component
      - include variant system
      - use react-router-don Link to navigate

--- src/components/Anchor/Anchor.tsx
+++ src/components/Anchor/Anchor.tsx
@@ -1,15 +1,20 @@
 import { Link } from "react-router-dom";
+import classes from "./anchor.module.css";
 
 interface AnchorProps {
   text: string;
-  to: string;
-  variant: "button" | "link";
+  to?: string;
+  variant?: "button" | "link";
 }
 
 const Anchor: React.FunctionComponent<AnchorProps> = ({
   text,
   to = "#",
   variant = "link",
-}) => <Link to={to}>{text}</Link>;
+}) => (
+  <Link to={to} className={`${classes.default} ${classes[variant]}`}>
+    {text}
+  </Link>
+);
 
 export default Anchor;
