723f55a0affcd4a7af8aaf88d3580129e93f0eb8
al-imam <alimam01828@gmail.com>
2022-12-26 20:27:11 +0600

    create SingUp component

--- src/components/SingUp/SingUp.tsx
+++ src/components/SingUp/SingUp.tsx
@@ -1,5 +1,23 @@
+import Anchor from "../Anchor/Anchor";
+import Button from "../Button/Button";
+import Form from "../Form/Form";
+import Input from "../Input/Input";
+import classes from "./singUp.module.css";
+
 const SingUp: React.FunctionComponent = () => {
-  return <h1>Your in SingUp</h1>;
+  return (
+    <Form>
+      <h1 className={classes.h1}>SingUp Form</h1>
+      <Input type="text" placeholder="Email" />
+      <Input type="password" placeholder="Password" />
+      <Input type="password" placeholder="Confirm password" />
+      <Button text="Sing Up" />
+      <p className={classes.p}>
+        Already have account?
+        <Anchor to="/login" text="Login" />
+      </p>
+    </Form>
+  );
 };
 
 export default SingUp;
