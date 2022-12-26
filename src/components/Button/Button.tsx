3e463198799e6db78bde9dad4f8169d2e3adc664
al-imam <alimam01828@gmail.com>
2022-12-26 15:16:55 +0600

    add button style

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,9 +1,13 @@
+import classes from "./button.module.css";
+
 interface ButtonProps {
   text: string;
 }
 
 const Button: React.FunctionComponent<ButtonProps> = ({ text }) => (
-  <button type="submit">{text}</button>
+  <button className={classes.Button} type="submit">
+    {text}
+  </button>
 );
 
 export default Button;
