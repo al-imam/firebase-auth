e4ca7cfadd14b4709c6ed95937f3254ded6a9361
al-imam <alimam01828@gmail.com>
2022-12-26 15:32:00 +0600

    add additional button style
      - add disabled button style
      - add loading button state

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -2,10 +2,14 @@ import classes from "./button.module.css";
 
 interface ButtonProps {
   text: string;
+  disable?: boolean;
 }
 
-const Button: React.FunctionComponent<ButtonProps> = ({ text }) => (
-  <button className={classes.Button} type="submit">
+const Button: React.FunctionComponent<ButtonProps> = ({
+  text,
+  disable = false,
+}) => (
+  <button className={classes.Button} disabled={disable} type="submit">
     {text}
   </button>
 );
