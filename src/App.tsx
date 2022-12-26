f3d24cffbe590b7961cbe43a41747a1143d690a9
al-imam <alimam01828@gmail.com>
2022-12-27 01:27:33 +0600

    wrap app with context provider

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,11 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./components/Home/Home";
 import Login from "./components/Login/Login";
 import SingUp from "./components/SingUp/SingUp";
+import { AuthProvider } from "./Context/AuthContext";
 
-const App: React.FunctionComponent = () => {
-  return (
-    <BrowserRouter>
+const App: React.FunctionComponent = () => (
+  <BrowserRouter>
+    <AuthProvider>
       <div className="container">
         <Routes>
           <Route path="/" element={<Home />} />
@@ -13,8 +14,8 @@ const App: React.FunctionComponent = () => {
           <Route path="/singup" element={<SingUp />} />
         </Routes>
       </div>
-    </BrowserRouter>
-  );
-};
+    </AuthProvider>
+  </BrowserRouter>
+);
 
 export default App;
