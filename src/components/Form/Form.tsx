1d8b15cf1f415b2f646e942e8f92ceea48aff4a9
al-imam <alimam01828@gmail.com>
2022-12-26 23:52:41 +0600

    take onSubmit function as pram

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -2,10 +2,13 @@ import classes from "./form.module.css";
 
 interface FormProps {
   children: React.ReactNode;
+  onSubmit?: React.FormEventHandler<HTMLFormElement>;
 }
 
-const Form: React.FunctionComponent<FormProps> = ({ children }) => (
-  <form className={classes.form}>{children}</form>
+const Form: React.FunctionComponent<FormProps> = ({ children, onSubmit }) => (
+  <form className={classes.form} onSubmit={onSubmit}>
+    {children}
+  </form>
 );
 
 export default Form;
