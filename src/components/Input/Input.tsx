c70a1c2349f060edf0014b79d8e1427bed3a2340
al-imam <alimam01828@gmail.com>
2022-12-26 20:39:49 +0600

    add name property
      - because we will grab data with that
      - it is optional

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -3,13 +3,19 @@ import classes from "./input.module.css";
 interface InputProps {
   type: string;
   placeholder: string;
+  name?: string;
 }
 
-const Input: React.FunctionComponent<InputProps> = ({ type, placeholder }) => (
+const Input: React.FunctionComponent<InputProps> = ({
+  type,
+  placeholder,
+  name,
+}) => (
   <input
     className={classes.inputs}
     type={type}
     placeholder={placeholder}
+    name={name}
     required
   />
 );
