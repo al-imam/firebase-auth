6d69e334d617ea27b038276ad73c0f08536f943a
al-imam <alimam01828@gmail.com>
2022-12-28 02:01:44 +0600

    implement public and private route

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,38 @@ import Home from "./components/Home/Home";
 import Login from "./components/Login/Login";
 import SingUp from "./components/SingUp/SingUp";
 import { AuthProvider } from "./Context/AuthContext";
+import PrivateRoute from "./Route/PrivateRoute";
+import PublicRoute from "./Route/PublicRoute";
 
 const App: React.FunctionComponent = () => (
   <BrowserRouter>
     <AuthProvider>
       <div className="container">
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/singup" element={<SingUp />} />
+          <Route
+            path="/"
+            element={
+              <PrivateRoute path="/singup">
+                <Home />
+              </PrivateRoute>
+            }
+          />
+          <Route
+            path="/login"
+            element={
+              <PublicRoute path="/">
+                <Login />
+              </PublicRoute>
+            }
+          />
+          <Route
+            path="/singup"
+            element={
+              <PublicRoute path="/">
+                <SingUp />
+              </PublicRoute>
+            }
+          />
         </Routes>
       </div>
     </AuthProvider>
