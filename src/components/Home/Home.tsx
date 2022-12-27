d8999a436970dc76ff9cdc172a8ad7aae41a3aab
al-imam <alimam01828@gmail.com>
2022-12-27 21:56:42 +0600

    design home page

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,12 +1,18 @@
+import Button from "../Button/Button";
+import Hr from "../Hr/Hr";
+import Anchor from "../Anchor/Anchor";
+import classes from "./home.module.css";
+import profile from "../../assets/bighead.svg";
+
 const Home: React.FunctionComponent = () => {
   return (
-    <h1
-      style={{
-        fontSize: "5rem",
-      }}
-    >
-      Your in home
-    </h1>
+    <div className={classes.home}>
+      <img src={profile} className={classes.img} />
+      <p className={classes.name}>Email - alimam01828@gmail.com</p>
+      <Button text="Update profile" />
+      <Hr />
+      <Anchor text="Log out" />
+    </div>
   );
 };
 
