3a647f860adf0b9277590b709fc10f0a66d030a7
al-imam <alimam01828@gmail.com>
2022-12-27 15:03:38 +0600

    take dispatch and value as a props and remove name attributes

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -3,19 +3,24 @@ import classes from "./input.module.css";
 interface InputProps {
   type: string;
   placeholder: string;
-  name?: string;
+  dispatch: (value: string) => void;
+  value: string;
 }
 
 const Input: React.FunctionComponent<InputProps> = ({
-  type,
+  type = "text",
   placeholder,
-  name,
+  dispatch,
+  value,
 }) => (
   <input
+    value={value}
+    onChange={(evt: React.ChangeEvent<HTMLInputElement>) =>
+      dispatch(evt.currentTarget.value)
+    }
     className={classes.inputs}
     type={type}
     placeholder={placeholder}
-    name={name}
     required
   />
 );
