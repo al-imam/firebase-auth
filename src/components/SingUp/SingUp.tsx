018d866ff436967c9f50e54bb149b761a7628768
al-imam <alimam01828@gmail.com>
2022-12-27 20:45:11 +0600

    validate manually
      - validate every possibility
      - and show corresponding message by Alert component

--- src/components/SingUp/SingUp.tsx
+++ src/components/SingUp/SingUp.tsx
@@ -3,11 +3,16 @@ import Anchor from "../Anchor/Anchor";
 import Button from "../Button/Button";
 import Form from "../Form/Form";
 import Input from "../Input/Input";
+import Alert from "../Alert/Alert";
 import classes from "./singUp.module.css";
 import { initializerArg, reducer } from "./reducer";
 import { useAuth } from "../../Context/AuthContext";
 
 const emailRegex = /^[a-zA-Z]([a-zA-Z0-9\.]){2,}@[a-z]{3,7}\.[a-z]{2,5}$/;
+
+function isEmpty(e: string, p: string, cp: string) {
+  return e.length === 0 || p.length === 0 || cp.length === 0;
+}
 
 const SingUp: React.FunctionComponent = () => {
   const [{ email, password, confirmPassword, error }, dispatch] = useReducer(
@@ -19,12 +24,19 @@ const SingUp: React.FunctionComponent = () => {
   async function onSubmit(evt: React.FormEvent) {
     evt.preventDefault();
 
+    if (isEmpty(email, password, confirmPassword)) {
+      return dispatch({
+        type: "error",
+        payload: "Email, password, confirm password required!",
+      });
+    }
+
     if (!email.match(emailRegex)) {
       return dispatch({ type: "error", payload: "Enter valid email address" });
     }
 
     if (password !== confirmPassword) {
-      return dispatch({ type: "error", payload: "Password not match" });
+      return dispatch({ type: "error", payload: "Passwords do not match" });
     }
 
     try {
@@ -39,7 +51,7 @@ const SingUp: React.FunctionComponent = () => {
   return (
     <Form onSubmit={onSubmit}>
       <h1 className={classes.h1}>SingUp Form</h1>
-      {error}
+      {error && <Alert message={error} />}
       <Input
         value={email}
         type="text"
