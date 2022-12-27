aab388aa9d8c3180b2131f060f86bfd2d9924df1
al-imam <alimam01828@gmail.com>
2022-12-27 16:58:04 +0600

    handle error

--- src/components/SingUp/SingUp.tsx
+++ src/components/SingUp/SingUp.tsx
@@ -5,16 +5,41 @@ import Form from "../Form/Form";
 import Input from "../Input/Input";
 import classes from "./singUp.module.css";
 import { initializerArg, reducer } from "./reducer";
+import { useAuth } from "../../Context/AuthContext";
+
+const emailRegex = /^[a-zA-Z]([a-zA-Z0-9\.]){2,}@[a-z]{3,7}\.[a-z]{2,5}$/;
 
 const SingUp: React.FunctionComponent = () => {
-  const [{ email, password, confirmPassword }, dispatch] = useReducer(
+  const [{ email, password, confirmPassword, error }, dispatch] = useReducer(
     reducer,
     initializerArg
   );
+  const { singUp } = useAuth()!;
+
+  async function onSubmit(evt: React.FormEvent) {
+    evt.preventDefault();
+
+    if (!email.match(emailRegex)) {
+      return dispatch({ type: "error", payload: "Enter valid email address" });
+    }
+
+    if (password !== confirmPassword) {
+      return dispatch({ type: "error", payload: "Password not match" });
+    }
+
+    try {
+      dispatch({ type: "error", payload: "" });
+      await singUp(email, password);
+    } catch (error) {
+      console.log(error);
+      return dispatch({ type: "error", payload: "something went wrong" });
+    }
+  }
 
   return (
-    <Form>
+    <Form onSubmit={onSubmit}>
       <h1 className={classes.h1}>SingUp Form</h1>
+      {error}
       <Input
         value={email}
         type="text"
