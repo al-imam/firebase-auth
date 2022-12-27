5d79a5f6c5c5d4dc1884c98839b2ff766eb6e9f9
al-imam <alimam01828@gmail.com>
2022-12-27 23:16:49 +0600

    show current auth user

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -3,12 +3,17 @@ import Hr from "../Hr/Hr";
 import Anchor from "../Anchor/Anchor";
 import classes from "./home.module.css";
 import profile from "../../assets/bighead.svg";
+import { useAuth } from "../../Context/AuthContext";
 
 const Home: React.FunctionComponent = () => {
+  const { currentUser } = useAuth()!;
+
   return (
     <div className={classes.home}>
       <img src={profile} className={classes.img} />
-      <p className={classes.name}>Email - alimam01828@gmail.com</p>
+      <p className={classes.name}>
+        {currentUser ? "Email - " + currentUser.email : "Loading......"}
+      </p>
       <Button text="Update profile" />
       <Hr />
       <Anchor text="Log out" />
