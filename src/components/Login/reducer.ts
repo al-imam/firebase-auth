32af0149eca7885088e6c7620768e820eaa11be7
al-imam <alimam01828@gmail.com>
2022-12-27 23:18:21 +0600

    create reducer function for form

---
+++ src/components/Login/reducer.ts
@@ -0,0 +1,51 @@
+interface InitState {
+  email: string;
+  password: string;
+  error: null | string;
+  loading: boolean;
+}
+
+export const initializerArg: InitState = {
+  email: "",
+  password: "",
+  error: null,
+  loading: false,
+};
+
+interface ACTION {
+  type: "email" | "password" | "error" | "loading";
+  payload: string;
+}
+
+export function reducer(state: InitState, action: ACTION) {
+  const { type, payload } = action;
+
+  switch (type) {
+    case "email":
+      return {
+        ...state,
+        email: payload,
+      };
+
+    case "password":
+      return {
+        ...state,
+        password: payload,
+      };
+
+    case "error":
+      return {
+        ...state,
+        error: payload.length === 0 ? null : payload,
+      };
+
+    case "loading":
+      return {
+        ...state,
+        loading: payload === "no" ? false : true,
+      };
+
+    default:
+      throw new Error("type don't exist ");
+  }
+}
