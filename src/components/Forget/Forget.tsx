bdd5fbf092a1d1edc70eee7bca90919dc4841208
al-imam <alimam01828@gmail.com>
2022-12-29 03:10:07 +0600

    Change some functionality
      - add success alert on success
      - disable button permanently when request is success
      - remove redirect behavior
      - add some additional danger alert

--- src/components/Forget/Forget.tsx
+++ src/components/Forget/Forget.tsx
@@ -1,5 +1,4 @@
 import { useState } from "react";
-import { useNavigate } from "react-router-dom";
 import { useAuth } from "../../Context/AuthContext";
 import { emailRegex } from "../SingUp/SingUp";
 import Alert from "../Alert/Alert";
@@ -13,19 +12,20 @@ import classes from "./forget.module.css";
 interface InitializerArg {
   loading: boolean;
   error: null | string;
+  success: string | null;
 }
 
 const initializerArg: InitializerArg = {
   loading: false,
   error: null,
+  success: null,
 };
 
 const Forget: React.FunctionComponent = () => {
   const [email, setEmail] = useState("");
-  const [{ loading, error }, setErrorAndLoading] =
+  const [{ loading, error, success }, setErrorAndLoading] =
     useState<InitializerArg>(initializerArg);
 
-  const navigate = useNavigate();
   const { resetPassword } = useAuth();
 
   async function onSubmit(evt: React.FormEvent) {
@@ -39,27 +39,44 @@ const Forget: React.FunctionComponent = () => {
     }
 
     try {
-      setErrorAndLoading((p) => ({ loading: true, error: null }));
-      await resetPassword(email);
-      navigate("/login", { replace: true });
-    } catch (error: any) {
-      setErrorAndLoading({ loading: false, error: "something went wrong" });
-    }
+      setErrorAndLoading((p) => ({
+        loading: true,
+        error: null,
+        success: null,
+      }));
 
-    setErrorAndLoading((p) => ({ ...p, loading: false }));
+      await resetPassword(email);
+
+      setErrorAndLoading((p) => ({
+        error: null,
+        loading: false,
+        success: "Password reset link sent on your email address",
+      }));
+    } catch (error: any) {
+      console.dir(error);
+      setErrorAndLoading({
+        loading: false,
+        error:
+          error.code === "auth/user-not-found"
+            ? "No account found with this email, singUp"
+            : "something went wrong",
+        success: null,
+      });
+    }
   }
 
   return (
     <Form onSubmit={onSubmit}>
       <h1 className={classes.h1}>Password reset</h1>
       {error && <Alert message={error} />}
+      {success && <Alert message={success} variant="success" />}
       <Input
         type="text"
         placeholder="Email address"
         value={email}
         dispatch={(value: string) => setEmail(value)}
       />
-      <Button text="Password reset" disable={loading} />
+      <Button text="Password reset" disable={loading || Boolean(success)} />
       <Anchor to="/login" text="Log In" />
       <Hr />
       <p className={classes.p}>
