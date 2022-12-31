c1ee98b18cec254bfd9cd4a3cdbac45effc8b0f7
al-imam <alimam01828@gmail.com>
2022-12-31 17:03:07 +0600

    create GoTo component for navigation and code reuse

---
+++ src/components/GoTO/GoTo.tsx
@@ -0,0 +1,23 @@
+import Anchor from "../Anchor/Anchor";
+import classes from "./goto.module.css";
+
+interface GoToProps {
+  text: string;
+  to: string;
+  anchorText: string;
+}
+
+const GoTo: React.FunctionComponent<GoToProps> = ({
+  text = "",
+  to,
+  anchorText,
+}) => {
+  return (
+    <p className={classes.p}>
+      {text}
+      <Anchor to={to} text={anchorText} />
+    </p>
+  );
+};
+
+export default GoTo;
