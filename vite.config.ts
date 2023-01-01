9da30a4733af4be0a54919d882f8e225ba4e78e7
al-imam <alimam01828@gmail.com>
2023-01-01 17:22:48 +0600

    map paths
      - @app <- src/*
      - @context <- src/Context/*
      - @routes <- src/Route/*
      - @components <- src/components/*
      - @pages <- src/components/pages/*
      - @utility <- src/components/utility/*
      - @assets <- src/assets/*
      - @helper <- src/helper/*

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
 import react from "@vitejs/plugin-react";
+import path from "path";
 import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      "@app": path.resolve(__dirname, "src"),
+      "@routes": path.resolve(__dirname, "src/Route"),
+      "@context": path.resolve(__dirname, "src/Context"),
+      "@components": path.resolve(__dirname, "src/components"),
+      "@utility": path.resolve(__dirname, "src/components/utility"),
+      "@pages": path.resolve(__dirname, "src/components/pages"),
+      "@helper": path.resolve(__dirname, "src/helper"),
+    },
+  },
 });
